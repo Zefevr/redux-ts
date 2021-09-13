4c2129689dd30277b8db1b1409dafdfc32cdd3d0
Zefevr <zefevillalba@gmail.com>
2021-09-13 10:30:24 +0200

    fix: useAction in package list

--- src/components/PackagesList.tsx
+++ src/components/PackagesList.tsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useActions } from "../hooks/useActions";
 
 const PackagesList: React.FC = () => {
   const [term, setTerm] = useState("");
+  const { searchPackages } = useActions();
+  const { data, error, loading } = useTypedSelector(state => state.packages);
+  console.log(data);
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    searchPackages(term);
   };
   return (
     <div>

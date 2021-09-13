aef36717677869312568a77cd0b35966fe84e802
Zefevr <zefevillalba@gmail.com>
2021-09-13 09:28:55 +0200

    fix: render app

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import { Provider } from "react-redux";
+import { store } from "../state";
+import PackagesList from "./PackagesList";
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for a Package</h1>
+        <PackagesList />
+      </div>
+    </Provider>
+  );
+};
+
+export default App;
